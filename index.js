c172e21b384fad943aed271ef80c5577e1fa13a6
Vincent Revard <Vincent.Revard@gmail.com>
2024-02-03 17:14:33 -0800

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+
+const findMatching = (drivers, entry) => {
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === entry.toLowerCase())
+    return matchingDrivers
+}
+
+const fuzzyMatch = (drivers, firstLetter) => {
+    const fuzzyDrivers = drivers.filter(driver => driver.startsWith(firstLetter))
+    return fuzzyDrivers
+    
+}
+
+const matchName = (drivers, firstName) => {
+    const matchingFirstName = drivers.filter(drivers => drivers.name === firstName)
+    return matchingFirstName
+}
+
